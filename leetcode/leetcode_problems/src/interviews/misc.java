6fa67224e5e77e2a9b90722ab5b89dcbbb0e05a6
Swaroop2020 <69762287+Swaroop2020@users.noreply.github.com>
2025-12-10 13:51:27 +0530

    Delete leetcode/leetcode_problems directory

--- leetcode/leetcode_problems/src/interviews/misc.java
+++
@@ -1,9 +0,0 @@
-package interviews;
-
-public class misc {
-	  public static void main(String[] args) {
-	      int digit = 2;
-	      char c = (char)(digit+'0');
-	      System.out.println(c);
-	  }
-}
